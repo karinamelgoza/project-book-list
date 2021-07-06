6b31f798563f50c598cab9251b90424ceec6ee43
Karina Melgoza <kmelgoza08@gmail.com>
2021-07-06 11:25:01 -0700

    adds public folder

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-// const path = require('path')
+const path = require('path')
 const ctrl = require('./controller')
 
 const { deleteBook, getAllBooks, addBook, shuffleBooks } = ctrl
@@ -14,13 +14,13 @@ app.delete('/booklist/:bookId', deleteBook)
 // app.get('/booklist', getAllBooks)
 app.get('/booklist/shuffle', shuffleBooks)
 
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../public/index.html'))
-// })
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+})
 
-// app.get('/styles', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../public/'))
-// })
+app.get('/styles', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.css'))
+})
 
 const serverPort = process.env.PORT || 5050
 app.listen(serverPort, () => {
