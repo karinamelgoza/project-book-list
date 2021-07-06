e79f2935acf50b5b0aa79a253d5411ee0f472e37
Karina Melgoza <kmelgoza08@gmail.com>
2021-07-06 11:18:29 -0700

    adds Procfile

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const path = require('path')
+// const path = require('path')
 const ctrl = require('./controller')
 
 const { deleteBook, getAllBooks, addBook, shuffleBooks } = ctrl
@@ -14,7 +14,15 @@ app.delete('/booklist/:bookId', deleteBook)
 // app.get('/booklist', getAllBooks)
 app.get('/booklist/shuffle', shuffleBooks)
 
-const serverPort = 5050
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../public/index.html'))
+// })
+
+// app.get('/styles', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../public/'))
+// })
+
+const serverPort = process.env.PORT || 5050
 app.listen(serverPort, () => {
     console.log(`server up on ${serverPort}`)
 })
